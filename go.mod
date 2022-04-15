7f4dd781b3c9bcc3b9a005e11682055228c76491
Ari Satrio <arisatrioputra03@gmail.com>
2022-04-15 16:37:09 +0700

    udpate major

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/arisatriop/new-module
+module github.com/arisatriop/new-module/v2
 
 go 1.17
--- say_hi.go
+++ say_hi.go
@@ -2,8 +2,8 @@ package gomodule2
 
 import "fmt"
 
-func SayHi(name string) string {
-	return fmt.Sprintf("Hi..., %s.\n", name)
+func SayHi(name string, me string) string {
+	return fmt.Sprintf("Hi, %s my name is %s.\n", name, me)
 }
 
 func SayHiWithName(name string) string {
